eff1b4e9a073fe4e14c5ac9cbf7cf605ccbf2e77
Tiberiu Zuld <tiberiuzuld@gmail.com>
2019-06-02 19:42:45 +0300

    update to angular material 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CgBusyModule} from 'angular-busy2';
 import 'hammerjs';
-import {
-  MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatGridListModule, MatInputModule,
-  MatSelectModule
-} from '@angular/material';
 
 import {AppComponent} from './app.component';
-import {CgBusyModule} from 'angular-busy2';
-import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
